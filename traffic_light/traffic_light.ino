2ed6ec9de1267622e5ea0f48c24fa8f95cccc5ab
DeppKepa <dasha.sisimirova@bk.ru>
2024-09-30 20:12:17 +0800

    Написала код для примитивного светофора

--- traffic_light/traffic_light.ino
+++ traffic_light/traffic_light.ino
@@ -1,8 +1,32 @@
+#define LED_PIN1 13
+#define LED_PIN2 12
+#define LED_PIN3 10
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED_PIN1, OUTPUT); 
+  pinMode(LED_PIN2, OUTPUT); 
+  pinMode(LED_PIN3, OUTPUT); 
 }
 
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  digitalWrite(LED_PIN1, HIGH);    // включаем светодиод
+	delay(10000);                   // ждем 1000 миллисекунд (1 секунда)
+	digitalWrite(LED_PIN1, LOW); 
+
+  digitalWrite(LED_PIN2, HIGH); 
+  delay(1000);                   // ждем 1000 миллисекунд (1 секунда)
+	digitalWrite(LED_PIN2, LOW);     // выключаем светодиод
+
+  digitalWrite(LED_PIN3, HIGH); 
+  delay(7000);                   // ждем 1000 миллисекунд (1 секунда)
+	digitalWrite(LED_PIN3, LOW); 
+
+  digitalWrite(LED_PIN2, HIGH); 
+  delay(1000);                   // ждем 1000 миллисекунд (1 секунда)
+	digitalWrite(LED_PIN2, LOW);
 }
