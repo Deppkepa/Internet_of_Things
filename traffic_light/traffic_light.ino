16c642867489024c2130835bef0e6ae2254b3673
AlhemyD <alhemyd@mail.ru>
2024-09-30 21:21:01 +0800

    Прописал начальную обработку команд с консоли. Определил переменные интервалов горения светодиодов

--- traffic_light/traffic_light.ino
+++ traffic_light/traffic_light.ino
@@ -5,8 +5,23 @@
 #define COMMAND_MANUAL 'm' //команда на ручное управление
 #define COMMAND_AUTO 'a' //команда на автоматическое управление
 
+#define TRAFFIC_AUTO 0
+#define TRAFFIC_MANUAL 1
+
 #define LED_RED 'r' //метка красного светодиода
 #define LED_GREEN 'g' //метка зелёного светодиода
+#define LED_YELLOW 'y' //метка жёлтого(синего) светодиода
+
+#define STATE_LED_RED 0
+#define STATE_LED_GREEN 1
+#define STATE_LED_YELLOW 2
+
+#define MANUAL_INTERVAL 60
+
+#define GREEN_USUAL_INTERVAL 10000
+#define YELLOW_USUAL_INTERVAL 1000
+#define YELLOW_UNCOMMON_INTERVAL 3000
+#define RED_USUAL_INTERVAL 7000
 
 /*
 Запрограммировать автоматический режим работы светофора:
@@ -19,8 +34,10 @@
  В коде не должно быть задержек (delay).
 */
 
-char traffic_status = 'a'; //статус по умолчанию - автоматическое управление
+int traffic_status=TRAFFIC_AUTO; //статус по умолчанию - автоматическое управление
 long int blink_start;
+char led;
+long int manual_start, green_start, yellow_start, red_start, green_blink_start;
 
 void setup() {
   // put your setup code here, to run once:
@@ -31,32 +48,69 @@ void setup() {
 }
 
 void update_state(){
-	return;
+	if (manual_start){
+		if (millis() <= manual_start+MANUAL_INTERVAL){
+			traffic_status=TRAFFIC_AUTO;
+		}
+	}
+	if (Serial.available() > 0) {
+		char cmd = Serial.read();
+		if(cmd == COMMAND_AUTO){
+			traffic_status=TRAFFIC_AUTO;
+		}
+		else if (cmd == COMMAND_MANUAL){
+			manual_start=millis();
+			traffic_status=TRAFFIC_MANUAL;
+		}
+		else if(cmd == LED_RED){
+			if (traffic_status==TRAFFIC_MANUAL){
+				led=STATE_LED_RED;
+			} else{
+				Serial.println("Cannot perform this command. - Auto mode");
+			}
+		}
+		else if (cmd ==LED_GREEN){
+			if (traffic_status==TRAFFIC_MANUAL){
+				led=STATE_LED_GREEN;
+			} else{
+				Serial.println("Cannot perform this command. - Auto mode");
+			}
+		} else{
+			Serial.println("Unknown command");
+		}
+	}
 }
 
+
 void process_state(){
-  //горит зелёный 10 секунд
-  digitalWrite(LED_PIN1, HIGH);// включаем светодиод
-  delay(10000);                // ждем 10 секунд
-  digitalWrite(LED_PIN1, LOW); // выключаем светодиод
+	if (traffic_status==TRAFFIC_AUTO){
+		//
+		//горит зелёный 10 секунд
+		digitalWrite(LED_PIN1, HIGH);// включаем светодиод
+		delay(10000);                // ждем 10 секунд
+		digitalWrite(LED_PIN1, LOW); // выключаем светодиод
   
-  //мигает зелёный 2 секунды
+		//мигает зелёный 2 секунды
   
-  //горит жёлтый (синий) 1 секунду
-  digitalWrite(LED_PIN2, HIGH); 
-  delay(1000);                   // ждем 1 секунду
-  digitalWrite(LED_PIN2, LOW);     
+		//горит жёлтый (синий) 1 секунду
+		digitalWrite(LED_PIN2, HIGH); 
+		delay(1000);                   // ждем 1 секунду
+		digitalWrite(LED_PIN2, LOW);     
 
-  //горит красный 7 секунд
-  digitalWrite(LED_PIN3, HIGH); 
-  delay(7000);                   // ждем 7 секунд
-  digitalWrite(LED_PIN3, LOW); 
+		//горит красный 7 секунд
+		digitalWrite(LED_PIN3, HIGH); 
+		delay(7000);                   // ждем 7 секунд
+		digitalWrite(LED_PIN3, LOW); 
 
-  //горит жёлтый 1 секунду
-  digitalWrite(LED_PIN2, HIGH); 
-  delay(1000);                   // ждем 1 секунду
-  digitalWrite(LED_PIN2, LOW);
-  return;
+		//горит жёлтый 1 секунду
+		digitalWrite(LED_PIN2, HIGH); 
+		delay(1000);                   // ждем 1 секунду
+		digitalWrite(LED_PIN2, LOW);
+	}
+	else if (traffic_status==TRAFFIC_MANUAL){
+		int k=0; 
+	}
+
 }
 
 void loop() {
