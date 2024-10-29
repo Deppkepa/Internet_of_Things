3dc3a821e8f37194fbd930b86f4ab141fb8844b0
AlhemyD <alhemyd@mail.ru>
2024-10-29 13:31:43 +0800

    Доработал алгоритм моргания. Задание выполнено

--- light_source_detector/light_source_detector.ino
+++ light_source_detector/light_source_detector.ino
@@ -1,48 +1,52 @@
 #define PHOTO_SENSOR_PIN1 A1
 #define PHOTO_SENSOR_PIN2 A2
-#define LED_PIN1 13
-#define LED_PIN2 12
+#define LED_PIN1 12
+#define LED_PIN2 13
 
-int calibr=165;
-bool blink_1,blink_2,blinking=true,true,true;
-unsigned long int sum=0, i=0;
-int ex=10;
+unsigned long previousMillis = 0;
+const long interval = 250;
 
 void setup() { 
   Serial.begin(9600); 
   pinMode(LED_PIN1,OUTPUT);
   pinMode(LED_PIN2,OUTPUT);
 } 
-
-void blink(){
-  if (blinking){
-    if (blink_1){
-      return;
-    }
-    if (blink_2){
-      return;
-    }
-  } else {
-    digitalWrite(LED_PIN1,HIGH);
-    digitalWrite(LED_PIN2,HIGH);
-  }
-}
  
 void loop() { 
-  int photo_val1 = analogRead(PHOTO_SENSOR_PIN1)-180; 
-  int photo_val2 = analogRead(PHOTO_SENSOR_PIN2)-30;
-  
-  if (photo_val1>=photo_val2 && abs(photo_val1-photo_val2)>ex){
-      blink_1=true;
-      blink_2=false;
-      blinking=true;
-  } else if(photo_val2>=photo_val1 && abs(photo_val1-photo_val2)>ex){
-    blink_2=true;
-    blink_1=false;
-    blinking=true;
-  } else{
-    blink_1,blink_2,blinking=false,false,false;
+  int lightValue1 = analogRead(PHOTO_SENSOR_PIN1)-180; 
+  int lightValue2 = analogRead(PHOTO_SENSOR_PIN2)-50;
+  unsigned long currentMillis = millis();
+ 
+  if (lightValue1 > lightValue2 + 10 || lightValue2 > lightValue1 + 10) {
+    
+    if (lightValue1 > lightValue2 + 10){
+      if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        digitalWrite(LED_PIN2,LOW);
+        if (digitalRead(LED_PIN1) == HIGH) {
+          digitalWrite(LED_PIN1, LOW);
+        } else {
+          digitalWrite(LED_PIN1, HIGH);
+        }
+        
+      }
+    } else{
+
+        if (currentMillis - previousMillis >= interval) {
+          previousMillis = currentMillis;
+          digitalWrite(LED_PIN1,LOW);
+          if (digitalRead(LED_PIN2) == HIGH) {
+            digitalWrite(LED_PIN2, LOW);
+          } else {
+            digitalWrite(LED_PIN2, HIGH);
+          }
+      }
+    }
   }
-  blink();  
+
+  if (lightValue1 <= lightValue2 + 10 && lightValue1 >= lightValue2 - 10) {
+    digitalWrite(LED_PIN1, HIGH);
+    digitalWrite(LED_PIN2, HIGH);
+  }
   
 }
