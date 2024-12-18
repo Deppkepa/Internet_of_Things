367437d9230ff7ff81c34c683a8e5e892e9ec93f
AlhemyD <alhemyd@mail.ru>
2024-12-19 00:26:36 +0800

    Доработал комплексные состояния. Задание выполнено

--- LedMatrix/LedMatrix.ino
+++ LedMatrix/LedMatrix.ino
@@ -9,12 +9,12 @@ on = 1 // 1 1 0 0
 2,4 = 7 // 1 1 1 0
 3,4 = 8 // 0 1 0 0
 1,3 = 9 // 1 1 0 1
-1,4 = 10 // combine(1\4)
-2,3 = 11 // combine(2\3)
-1,2,4 = 12 // combine(1,2\4)
-2,3,4 = 13 // combine(2,4\3)
-3,4,1 = 14 // combine(3,4\1)
-1,3,2 = 15 // combine(1,3\2)
+1,4 = ! // combine(1\4)
+2,3 = @ // combine(2\3)
+1,2,4 = # // combine(1,2\4)
+2,3,4 = $ // combine(2,4\3)
+3,4,1 = % // combine(3,4\1)
+1,3,2 = ^ // combine(1,3\2)
 
 */
 
@@ -26,6 +26,9 @@ on = 1 // 1 1 0 0
 
 int state=0;
 int previous_state=state;
+uint32_t blink_start=0;
+int current=1;
+int previous_current=current;
 
 void setup() {
   Serial.begin(9600);
@@ -79,27 +82,27 @@ void update_state(){
         previous_state=state;
         state=9;
         break;
-      case '10': //1,4
+      case '!': //1,4
         previous_state=state;
         state=10;
         break;
-      case '11': //2,3
+      case '@': //2,3
         previous_state=state;
         state=11;
         break;
-      case '12': //1,2,4
+      case '#': //1,2,4
         previous_state=state;
         state=12;
         break;
-      case '13': //2,3,4
+      case '$': //2,3,4
         previous_state=state;
         state=13;
         break;
-      case '14': //3,4,1
+      case '%': //3,4,1
         previous_state=state;
         state=14;
         break;
-      case '15': //1,3,2
+      case '^': //1,3,2
         previous_state=state;
         state=15;
         break;
@@ -245,16 +248,318 @@ void process_state(){
       } 
       break;
     case 10: //1,4
+      if (previous_state!=10){
+        analogWrite(FIRST_PIN,0);
+        analogWrite(SECOND_PIN,0);
+        analogWrite(THIRD_PIN,0);
+        analogWrite(FOURTH_PIN,0);
+        previous_state=10;
+      } else{
+        switch(current){
+          case 1:
+            if (previous_current!=1){
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,0);
+              previous_current=1;
+              blink_start=millis();
+            } else {
+              
+              
+              analogWrite(FIRST_PIN,255);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,255);
+            }
+            if(millis()-blink_start>=1){
+              current=2;
+            }
+            break;
+          case 2:
+            if (previous_current!=2){
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,0);
+              previous_current=2;
+              blink_start=millis();
+            } else {
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,255);
+              analogWrite(THIRD_PIN,255);
+              analogWrite(FOURTH_PIN,0);
+              
+            }
+            if(millis()-blink_start>=1){
+              current=1;
+            }
+            break;
+          default:
+            break;
+        }        
+        /*1,4 = ! // combine(1\4)*/
+      }   
       break;
     case 11: //2,3
+      if (previous_state!=11){
+        analogWrite(FIRST_PIN,0);
+        analogWrite(SECOND_PIN,0);
+        analogWrite(THIRD_PIN,0);
+        analogWrite(FOURTH_PIN,0);
+        previous_state=11;
+      } else{
+        /*2,3 = @ // combine(2\3)*/
+        switch(current){
+          case 1:
+            if (previous_current!=1){
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,0);
+              previous_current=1;
+              blink_start=millis();
+            } else {
+              analogWrite(FIRST_PIN,255);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,255);
+              analogWrite(FOURTH_PIN,0);
+            }
+            if(millis()-blink_start>=1){
+              current=2;
+            }
+            break;
+          case 2:
+            if (previous_current!=2){
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,0);
+              previous_current=2;
+              blink_start=millis();
+            } else {
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,255);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,255);
+              
+            }
+            if(millis()-blink_start>=1){
+              current=1;
+            }
+            break;
+          default:
+            break;
+        }   
+      }   
       break;
     case 12: //1,2,4
+      if (previous_state!=12){
+        analogWrite(FIRST_PIN,0);
+        analogWrite(SECOND_PIN,0);
+        analogWrite(THIRD_PIN,0);
+        analogWrite(FOURTH_PIN,0);
+        previous_state=12;
+      } else{
+        /*1,2,4 = # // combine(1,2\4)*/
+        switch(current){
+          case 1:
+            if (previous_current!=1){
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,0);
+              previous_current=1;
+              blink_start=millis();
+            } else {
+              analogWrite(FIRST_PIN,255);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,0);
+            }
+            if(millis()-blink_start>=1){
+              current=2;
+            }
+            break;
+          case 2:
+            if (previous_current!=2){
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,0);
+              previous_current=2;
+              blink_start=millis();
+            } else {
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,255);
+              analogWrite(THIRD_PIN,255);
+              analogWrite(FOURTH_PIN,0);
+              
+            }
+            if(millis()-blink_start>=1){
+              current=1;
+            }
+            break;
+          default:
+            break;
+        }  
+      }   
       break;
     case 13: //2,3,4
+      if (previous_state!=13){
+        analogWrite(FIRST_PIN,0);
+        analogWrite(SECOND_PIN,0);
+        analogWrite(THIRD_PIN,0);
+        analogWrite(FOURTH_PIN,0);
+        previous_state=13;
+      } else{
+        /*1,4 = ! //2,3,4 = $ // combine(2,4\3)*/
+        switch(current){
+          case 1:
+            if (previous_current!=1){
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,0);
+              previous_current=1;
+              blink_start=millis();
+            } else {
+              analogWrite(FIRST_PIN,255);
+              analogWrite(SECOND_PIN,255);
+              analogWrite(THIRD_PIN,255);
+              analogWrite(FOURTH_PIN,0);
+            }
+            if(millis()-blink_start>=1){
+              current=2;
+            }
+            break;
+          case 2:
+            if (previous_current!=2){
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,0);
+              previous_current=2;
+              blink_start=millis();
+            } else {
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,255);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,255);
+              
+            }
+            if(millis()-blink_start>=1){
+              current=1;
+            }
+            break;
+          default:
+            break;
+        }  
+      }  
       break;
     case 14: //3,4,1
+      if (previous_state!=14){
+        analogWrite(FIRST_PIN,0);
+        analogWrite(SECOND_PIN,0);
+        analogWrite(THIRD_PIN,0);
+        analogWrite(FOURTH_PIN,0);
+        previous_state=14;
+      } else{
+        /*3,4,1 = % // combine(3,4\1)*/
+        switch(current){
+          case 1:
+            if (previous_current!=1){
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,0);
+              previous_current=1;
+              blink_start=millis();
+            } else {
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,255);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,0);
+            }
+            if(millis()-blink_start>=1){
+              current=2;
+            }
+            break;
+          case 2:
+            if (previous_current!=2){
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,0);
+              previous_current=2;
+              blink_start=millis();
+            } else {
+              analogWrite(FIRST_PIN,255);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,255);
+              
+            }
+            if(millis()-blink_start>=1){
+              current=1;
+            }
+            break;
+          default:
+            break;
+        }  
+      }  
       break;
     case 15: //1,3,2
+      if (previous_state!=15){
+        analogWrite(FIRST_PIN,0);
+        analogWrite(SECOND_PIN,0);
+        analogWrite(THIRD_PIN,0);
+        analogWrite(FOURTH_PIN,0);
+        previous_state=15;
+      } else{
+        /*1,3,2 = ^ // combine(1,3\2)*/
+        switch(current){
+          case 1:
+            if (previous_current!=1){
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,0);
+              previous_current=1;
+              blink_start=millis();
+            } else {
+              analogWrite(FIRST_PIN,255);
+              analogWrite(SECOND_PIN,255);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,255);
+            }
+            if(millis()-blink_start>=1){
+              current=2;
+            }
+            break;
+          case 2:
+            if (previous_current!=2){
+              analogWrite(FIRST_PIN,0);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,0);
+              analogWrite(FOURTH_PIN,0);
+              previous_current=2;
+              blink_start=millis();
+            } else {
+              analogWrite(FIRST_PIN,255);
+              analogWrite(SECOND_PIN,0);
+              analogWrite(THIRD_PIN,255);
+              analogWrite(FOURTH_PIN,0);
+              
+            }
+            if(millis()-blink_start>=1){
+              current=1;
+            }
+            break;
+          default:
+            break;
+        }  
+      }  
       break;
     default:
       break;
